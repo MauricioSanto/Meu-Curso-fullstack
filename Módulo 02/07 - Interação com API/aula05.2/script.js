1eafb24c3a7c2fac70f4268daeea5801f740fb7f
MauricioSanto <mauriciosantosnet@gmail.com>
2024-04-03 11:19:41 -0300

    arquivos modificados

--- Módulo 02/07 - Interação com API/aula05.2/script.js
+++ Módulo 02/07 - Interação com API/aula05.2/script.js
@@ -8,20 +8,17 @@ async function busca(){
         listdiv.innerHTML += `
             <div class="card" data-id = "${item.id}">
                 <div class="grupo-img">
-                    <img src="${item.img[0]}" width="250" height="auto">
+                    <img src="${item.img[0]}" width="250" height="auto" >
                 </div>
                 <div class="textos">
                     <h3>${item.nome}</h3>
                     <p> Descrição: ${item.Descrição}</p>
-                    
                     <p> Valor R$ ${(item.Preço).toFixed(2).replace(".",",")}</p>
                     <p> Ano ${item.Ano}</P>
                     <p> KM ${item.Quilometragem}</p>
-                        
-                    
-
                 </div>
             </div>
+         
         `
     }
     
