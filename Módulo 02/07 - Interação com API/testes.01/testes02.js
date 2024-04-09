3bd5b4100e34277b0f340d27fab6a40c7ce1ae5b
Mauricio Santo <mauriciosantosnet@gmail.com>
2024-04-09 11:22:49 -0300

    add de arquivos

--- Módulo 02/07 - Interação com API/testes.01/testes02.js
+++ Módulo 02/07 - Interação com API/testes.01/testes02.js
@@ -1,19 +1,43 @@
-// script.js
-const prevBtn = document.getElementById('prevBtn');
-const nextBtn = document.getElementById('nextBtn');
-const cardList = document.querySelector('.card-list');
-let currentIndex = 0;
+/*
+function classToggle() {
+    var el = document.querySelector('.icon-cards__content');
+    el.classList.toggle('step-animation');
+  }
+  
+  document.querySelector('#toggle-animation').addEventListener('click', classToggle);
+  */
+/*
+$('.slider').slick({
+    arrows: true,
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true,
+    draggable: false
+  });
+  
+  $('.slider')
+    .on('beforeChange', function(event, slick, currentSlide, nextSlide){
+      $('.slick-list').addClass('do-transition')
+    })
+    .on('afterChange', function(){
+      $('.slick-list').removeClass('do-transition')
+    });
+    */
+    const carousel = document.querySelector('.carousel');
+    const cards = document.querySelectorAll('.card');
+    const cardWidth = cards[0].offsetWidth;
+    let currentIndex = 0;
 
-prevBtn.addEventListener('click', () => {
-    currentIndex = (currentIndex - 1 + cardList.children.length) % cardList.children.length;
-    updateCarousel();
-});
+    function moveCarousel() {
+        currentIndex++;
+        if (currentIndex >= cards.length) {
+            currentIndex = 0;
+        }
+        const translateX = -currentIndex * cardWidth;
+        carousel.style.transform = `translateX(${translateX}px)`;
+    }
 
-nextBtn.addEventListener('click', () => {
-    currentIndex = (currentIndex + 1) % cardList.children.length;
-    updateCarousel();
-});
-
-function updateCarousel() {
-    cardList.style.transform = `translateX(-${currentIndex * 100}%)`;
-}
+    setInterval(moveCarousel, 3000); // Altere o intervalo conforme desejad
