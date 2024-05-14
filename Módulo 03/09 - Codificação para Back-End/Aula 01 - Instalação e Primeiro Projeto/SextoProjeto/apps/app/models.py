65075a1e9ebcdc9a31bc24a28b403fabd1bedf05
Mauricio Santo <mauriciosantosnet@gmail.com>
2024-05-14 11:08:26 -0300

    ad de arquivos

---
+++ Módulo 03/09 - Codificação para Back-End/Aula 01 - Instalação e Primeiro Projeto/SextoProjeto/apps/app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class roupas(models.Model):
+    nome = models.CharField(max_length=50)
+    tipo = models.CharField(max_length=50)
+
+
+
+
+
+
+
---
+++ Módulo 03/09 - Codificação para Back-End/Aula 01 - Instalação e Primeiro Projeto/SextoProjeto/apps/rodas/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RodasConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'rodas'
