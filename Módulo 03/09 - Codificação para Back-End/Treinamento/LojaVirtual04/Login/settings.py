0fbfef5ea80b759987cda7b620f63aebb05de924
Mauricio Santo <mauriciosantosnet@gmail.com>
2024-05-31 11:15:23 -0300

    ad de arquivos

--- Módulo 03/09 - Codificação para Back-End/Treinamento/LojaVirtual04/Login/settings.py
+++ Módulo 03/09 - Codificação para Back-End/Treinamento/LojaVirtual04/Login/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.Senha',
+    'apps.Cliente',
 ]
 
 MIDDLEWARE = [
---
+++ Módulo 03/09 - Codificação para Back-End/Treinamento/LojaVirtual04/apps/Cliente/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class cliente (models.Model):
+    nome = models.CharField(max_length=100, verbose_name='Nome')
+    cpf = models.PositiveIntegerField(verbose_name='CPF')
+
+    def __str__(self):
+        return self.nome
+    
