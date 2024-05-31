9b7b0baadfc1466d0aeafd10ca6d41f0cbba2b83
Mauricio Santo <mauriciosantosnet@gmail.com>
2024-05-31 11:01:13 -0300

    ad de arquivos

---
+++ Módulo 03/09 - Codificação para Back-End/Treinamento/LojaVirtual04/apps/Senha/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SenhaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.Senha'
---
+++ Módulo 03/09 - Codificação para Back-End/Treinamento/LojaVirtual04/apps/Senha/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import produto
+
+def VerProdutos(request):
+    produtos_lista = produto.objects.all()
+    return render(request, 'index.html', {'produtos': produtos_lista})
+
