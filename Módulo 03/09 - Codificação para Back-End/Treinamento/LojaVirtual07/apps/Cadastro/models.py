719fe271bc3e7e14f6ae7fc0fe51111c2667c1d6
Mauricio Santo <mauriciosantosnet@gmail.com>
2024-06-13 11:18:33 -0300

    ad de arquivos

--- Módulo 03/09 - Codificação para Back-End/Treinamento/LojaVirtual07/apps/Cadastro/models.py
+++ Módulo 03/09 - Codificação para Back-End/Treinamento/LojaVirtual07/apps/Cadastro/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class cadastro (models.Model):
     nome = models.CharField(max_length=100)
     email = models.EmailField(verbose_name="E-Mail")
-    foto = models.ImageField(upload_to="foto_perfil/", default=True)
-    celular = models.IntegerField(default=True)
+    foto = models.ImageField(upload_to="foto_perfil/", null=True, blank=True)
+    celular = models.IntegerField()
 
     def __str__(self):
         return self.nome
@@ -12,7 +12,8 @@ class cadastro (models.Model):
 class clientes(models.Model):
     nome = models.CharField(max_length=50)
     email = models.EmailField(verbose_name='E-mail')
-    contato = models.IntegerField(default=True)
+    contato = models.IntegerField()
+    
 
     def __str__(self):
         return self.nome
@@ -21,7 +22,7 @@ class carros (models.Model):
     modelo = models.CharField(max_length=50)
     fabricante = models.CharField(max_length=50)
     ano = models.IntegerField()
-    foto = models.ImageField(upload_to="foto_perfil/", default=True)
+    foto = models.ImageField(upload_to="foto_perfil/", null=True , blank=True)
 
     def __str__(self):
         return self.modelo
@@ -29,6 +30,8 @@ class carros (models.Model):
 class pagamento(models.Model):
     forma_de_pagamento = models.CharField(max_length=50)
     valor = models.DecimalField(max_digits=10, decimal_places=2)
+    carro = models.ForeignKey(to=carros, on_delete=models.CASCADE, null=True, blank=True)
+    cliente = models.OneToOneField(clientes,on_delete=models.CASCADE,null=True, blank=True)
 
     def __str__(self):
         return self.forma_de_pagamento + '|' + str(self.valor)
