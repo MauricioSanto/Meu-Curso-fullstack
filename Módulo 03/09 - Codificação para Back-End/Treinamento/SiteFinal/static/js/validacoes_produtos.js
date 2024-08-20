58cec3dc603e261a295ef174cc0fc35f32c1710c
Mauricio santo <mauriciosantosnet@gmail.com>
2024-08-20 11:29:41 -0300

    ad de arquivos

--- Módulo 03/09 - Codificação para Back-End/Treinamento/SiteFinal/static/js/validacoes_produtos.js
+++ Módulo 03/09 - Codificação para Back-End/Treinamento/SiteFinal/static/js/validacoes_produtos.js
@@ -1,9 +1,19 @@
 $(document).ready(function(){
     
     $('#form_produto').submit(function(event){
-        let nome = $('#id_produto').val()
+        let nome = $('#nome').val()
+        let valor = $('#valor').val()
+        let descricao = $('#descricao').val()
         if(nome ==''){
             alert('Nome Obrigatório!')
+            event.preventDefault();
+        }
+        if(valor ==''){
+            alert('Valor Obrigatório!')
+            event.preventDefault();
+        }
+        if(descricao ==''){
+            alert('Descrição Obrigatório!')
             event.preventDefault();
         }
     });
