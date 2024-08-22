99c3ad636353b0bb02043bbb79059e61adad2357
Mauricio santo <mauriciosantosnet@gmail.com>
2024-08-22 11:22:13 -0300

    ad de arquivos

--- Módulo 03/09 - Codificação para Back-End/Treinamento/SiteFinal/apps/core/views.py
+++ Módulo 03/09 - Codificação para Back-End/Treinamento/SiteFinal/apps/core/views.py
@@ -33,177 +33,177 @@ def VerIndex(request):
 
 
 #def CriarCliente(request):
-    busca_clientes = Cliente.objects.all()
+   # busca_clientes = Cliente.objects.all()
     
-    if request.method == "GET":
+    #if request.method == "GET":
         novo_cliente = FormularioCliente()
-    else:
-        cliente_preenchido = FormularioCliente(request.POST, request.FILES)
-        if cliente_preenchido.is_valid():
-            cliente_preenchido.save()
-            return redirect("pg_criar_cliente")
-    return render(request, "form-cliente.html", {"form_cliente": novo_cliente, "clientes": busca_clientes})
+    #else:
+        #cliente_preenchido = FormularioCliente(request.POST, request.FILES)
+        #if cliente_preenchido.is_valid():
+            #cliente_preenchido.save()
+           # return redirect("pg_criar_cliente")
+    #return render(request, "form-cliente.html", {"form_cliente": novo_cliente, "clientes": busca_clientes})
 
 #def EditarCliente(request, id_cliente):
-    busca_cliente = Cliente.objects.get(id=id_cliente)
-    if request.method == "GET":
-        editar_cliente = FormularioCliente(instance=busca_cliente)
-    else:
-        cliente_editado = FormularioCliente(request.POST, instance=busca_cliente)
-        if cliente_editado.is_valid():
-            cliente_editado.save()
-            return redirect("pg_criar_cliente")
-    return render(request, "form-cliente.html", {"form_cliente": editar_cliente})
+    #busca_cliente = Cliente.objects.get(id=id_cliente)
+    #if request.method == "GET":
+        #editar_cliente = FormularioCliente(instance=busca_cliente)
+   # else:
+        #cliente_editado = FormularioCliente(request.POST, instance=busca_cliente)
+       # if cliente_editado.is_valid():
+           # cliente_editado.save()
+            #return redirect("pg_criar_cliente")
+    #return render(request, "form-cliente.html", {"form_cliente": editar_cliente})
 
 #def ExcluirCliente(request, id_cliente):
-    busca_cliente = Cliente.objects.get(id=id_cliente)
-    if request.method == "POST":
-        busca_cliente.delete()
-        return redirect("pg_criar_cliente")
-    titulo_objeto = busca_cliente.nome
-    return render(request, "conf-excluir.html", {"valor": titulo_objeto})
+    # busca_cliente = Cliente.objects.get(id=id_cliente)
+    # if request.method == "POST":
+    #     busca_cliente.delete()
+    #     return redirect("pg_criar_cliente")
+    # titulo_objeto = busca_cliente.nome
+    # return render(request, "conf-excluir.html", {"valor": titulo_objeto})
 
 #def CriarEmpresa(request):
-    busca_empresas = Empresa.objects.all()
+    # busca_empresas = Empresa.objects.all()
     
-    if request.method == "GET":
-        nova_empresa = FormularioEmpresa()
-    else:
-        empresa_preenchida = FormularioEmpresa(request.POST)
-        if empresa_preenchida.is_valid():
-            empresa_preenchida.save()
-            return redirect("pg_criar_empresa")
-    return render(request, "form-empresa.html", {"form_empresa": nova_empresa, "empresas": busca_empresas})
+    # if request.method == "GET":
+    #     nova_empresa = FormularioEmpresa()
+    # else:
+    #     empresa_preenchida = FormularioEmpresa(request.POST)
+    #     if empresa_preenchida.is_valid():
+    #         empresa_preenchida.save()
+    #         return redirect("pg_criar_empresa")
+    # return render(request, "form-empresa.html", {"form_empresa": nova_empresa, "empresas": busca_empresas})
 
 #def EditarEmpresa(request, id_empresa):
-    busca_empresa = Empresa.objects.get(id=id_empresa)
-    if request.method == "GET":
-        editar_empresa = FormularioEmpresa(instance=busca_empresa)
-    else:
-        empresa_editada = FormularioEmpresa(request.POST, instance=busca_empresa)
-        if empresa_editada.is_valid():
-            empresa_editada.save()
-            return redirect("pg_criar_empresa")
-    return render(request, "form-empresa.html", {"form_empresa": editar_empresa})
+    # busca_empresa = Empresa.objects.get(id=id_empresa)
+    # if request.method == "GET":
+    #     editar_empresa = FormularioEmpresa(instance=busca_empresa)
+    # else:
+    #     empresa_editada = FormularioEmpresa(request.POST, instance=busca_empresa)
+    #     if empresa_editada.is_valid():
+    #         empresa_editada.save()
+    #         return redirect("pg_criar_empresa")
+    # return render(request, "form-empresa.html", {"form_empresa": editar_empresa})
 
 #def ExcluirEmpresa(request, id_empresa):
-    busca_empresa = Empresa.objects.get(id=id_empresa)
-    if request.method == "POST":
-        busca_empresa.delete()
-        return redirect("pg_criar_empresa")
-    titulo_objeto = busca_empresa.razao_social
-    return render(request, "conf-excluir.html", {"valor": titulo_objeto})
+    # busca_empresa = Empresa.objects.get(id=id_empresa)
+    # if request.method == "POST":
+    #     busca_empresa.delete()
+    #     return redirect("pg_criar_empresa")
+    # titulo_objeto = busca_empresa.razao_social
+    # return render(request, "conf-excluir.html", {"valor": titulo_objeto})
 
 #def CriarServico(request):
-    busca_servicos = Servico.objects.all()
+    # busca_servicos = Servico.objects.all()
     
-    if request.method == "GET":
-        novo_servico = FormularioServico()
-    else:
-        servico_preenchido = FormularioServico(request.POST)
-        if servico_preenchido.is_valid():
-            servico_preenchido.save()
-            return redirect("pg_criar_servico")
-    return render(request, "form-servico.html", {"form_servico": novo_servico, "servicos": busca_servicos})
+    # if request.method == "GET":
+    #     novo_servico = FormularioServico()
+    # else:
+    #     servico_preenchido = FormularioServico(request.POST)
+    #     if servico_preenchido.is_valid():
+    #         servico_preenchido.save()
+    #         return redirect("pg_criar_servico")
+    # return render(request, "form-servico.html", {"form_servico": novo_servico, "servicos": busca_servicos})
 
-def EditarServico(request, id_servico):
-    busca_servico = Servico.objects.get(id=id_servico)
-    if request.method == "GET":
-        editar_servico = FormularioServico(instance=busca_servico)
-    else:
-        servico_editado = FormularioServico(request.POST, instance=busca_servico)
-        if servico_editado.is_valid():
-            servico_editado.save()
-            return redirect("pg_criar_servico")
-    return render(request, "form-servico.html", {"form_servico": editar_servico})
+# def EditarServico(request, id_servico):
+#     busca_servico = Servico.objects.get(id=id_servico)
+#      if request.method == "GET":
+#         editar_servico = FormularioServico(instance=busca_servico)
+#     else:
+#        servico_editado = FormularioServico(request.POST, instance=busca_servico)
+#          if servico_editado.is_valid():
+#              servico_editado.save()
+#              return redirect("pg_criar_servico")
+#     return render(request, "form-servico.html", {"form_servico": editar_servico})
 
 #def ExcluirServico(request, id_servico):
-    busca_servico = Servico.objects.get(id=id_servico)
-    if request.method == "POST":
-        busca_servico.delete()
-        return redirect("pg_criar_servico")
-    titulo_objeto = busca_servico.tipo_servico
-    return render(request, "conf-excluir.html", {"valor": titulo_objeto})
+    # busca_servico = Servico.objects.get(id=id_servico)
+    # if request.method == "POST":
+    #     busca_servico.delete()
+    #     return redirect("pg_criar_servico")
+    # titulo_objeto = busca_servico.tipo_servico
+    # return render(request, "conf-excluir.html", {"valor": titulo_objeto})
 
 #def CriarCategoria(request):
-    busca_categorias = Categoria.objects.all()
+    # busca_categorias = Categoria.objects.all()
     
-    if request.method == "GET":
-        nova_categoria = FormularioCategoria()
-    else:
-        categoria_preenchida = FormularioCategoria(request.POST)
-        if categoria_preenchida.is_valid():
-            categoria_preenchida.save()
-            return redirect("pg_criar_categoria")
-    return render(request, "form-categoria.html", {"form_categoria": nova_categoria, "categorias": busca_categorias})
+    # if request.method == "GET":
+    #     nova_categoria = FormularioCategoria()
+    # else:
+    #     categoria_preenchida = FormularioCategoria(request.POST)
+    #     if categoria_preenchida.is_valid():
+    #         categoria_preenchida.save()
+    #         return redirect("pg_criar_categoria")
+    # return render(request, "form-categoria.html", {"form_categoria": nova_categoria, "categorias": busca_categorias})
 
 #def EditarCategoria(request, id_categoria):
-    busca_categoria = Categoria.objects.get(id=id_categoria)
-    if request.method == "GET":
-        editar_categoria = FormularioCategoria(instance=busca_categoria)
-    else:
-        categoria_editada = FormularioCategoria(request.POST, instance=busca_categoria)
-        if categoria_editada.is_valid():
-            categoria_editada.save()
-            return redirect("pg_criar_categoria")
-    return render(request, "form-categoria.html", {"form_categoria": editar_categoria})
+    # busca_categoria = Categoria.objects.get(id=id_categoria)
+    # if request.method == "GET":
+    #     editar_categoria = FormularioCategoria(instance=busca_categoria)
+    # else:
+    #     categoria_editada = FormularioCategoria(request.POST, instance=busca_categoria)
+    #     if categoria_editada.is_valid():
+    #         categoria_editada.save()
+    #         return redirect("pg_criar_categoria")
+    # return render(request, "form-categoria.html", {"form_categoria": editar_categoria})
 
 #def ExcluirCategoria(request, id_categoria):
-    busca_categoria = Categoria.objects.get(id=id_categoria)
-    if request.method == "POST":
-        busca_categoria.delete()
-        return redirect("pg_criar_categoria")
-    titulo_objeto = busca_categoria.tipo
-    return render(request, "conf-excluir.html", {"valor": titulo_objeto})
+    # busca_categoria = Categoria.objects.get(id=id_categoria)
+    # if request.method == "POST":
+    #     busca_categoria.delete()
+    #     return redirect("pg_criar_categoria")
+    # titulo_objeto = busca_categoria.tipo
+    # return render(request, "conf-excluir.html", {"valor": titulo_objeto})
 
 #def CriarOrdemServico(request):
-    if request.method == "GET":
-        nova_ordemservico = FormularioOrdemServico()
-    else:
-        ordemservico_preenchida = FormularioOrdemServico(request.POST)
-        if ordemservico_preenchida.is_valid():
-            ordemservico_preenchida.save()
-            return redirect("pg_inicial")
-    return render(request, "form-ordemservico.html", {"form_ordemservico": nova_ordemservico})
+    # if request.method == "GET":
+    #     nova_ordemservico = FormularioOrdemServico()
+    # else:
+    #     ordemservico_preenchida = FormularioOrdemServico(request.POST)
+    #     if ordemservico_preenchida.is_valid():
+    #         ordemservico_preenchida.save()
+    #         return redirect("pg_inicial")
+    # return render(request, "form-ordemservico.html", {"form_ordemservico": nova_ordemservico})
 
 #def ExcluirOrdemServico(request, id_os):
-    busca_os = OrdemServico.objects.get(id=id_os)
-    if request.method == "POST":
-        busca_os.delete()
-        return redirect("pg_inicial")
-    titulo_objeto = "OS: " + str(busca_os.id) + " | " + busca_os.cliente.nome
-    return render(request, "conf-excluir.html", {"valor": titulo_objeto})
+    # busca_os = OrdemServico.objects.get(id=id_os)
+    # if request.method == "POST":
+    #     busca_os.delete()
+    #     return redirect("pg_inicial")
+    # titulo_objeto = "OS: " + str(busca_os.id) + " | " + busca_os.cliente.nome
+    # return render(request, "conf-excluir.html", {"valor": titulo_objeto})
 
 #def CriarProduto(request):
-    busca_produto = Produto.objects.all()
+    #busca_produto = Produto.objects.all()
     
-    if request.method == "GET":
-        novo_produto = FormularioProduto()
-    else:
-        produto_preenchido = FormularioProduto(request.POST, request.FILES)
-        if produto_preenchido.is_valid():
-            produto_preenchido.save()
-            return redirect("pg_criar_produto")
-    return render(request, "form-produtos.html", {"form_produto": novo_produto, "produtos": busca_produto})
+#     if request.method == "GET":
+#         novo_produto = FormularioProduto()
+#     else:
+#         produto_preenchido = FormularioProduto(request.POST, request.FILES)
+#         if produto_preenchido.is_valid():
+#             produto_preenchido.save()
+#             return redirect("pg_criar_produto")
+#     return render(request, "form-produtos.html", {"form_produto": novo_produto, "produtos": busca_produto})
 
-#def EditarProduto(request, id_produto):
-    busca_produto = Produto.objects.get(id=id_produto)
-    if request.method == "GET":
-        editar_produto = FormularioProduto(instance=busca_produto)
-    else:
-        produto_editado = FormularioProduto(request.POST, instance=busca_produto)
-        if produto_editado.is_valid():
-            produto_editado.save()
-            return redirect("pg_criar_produto")
-    return render(request, "form-produtos.html", {"form_produto": editar_produto})
+# #def EditarProduto(request, id_produto):
+    # busca_produto = Produto.objects.get(id=id_produto)
+    # if request.method == "GET":
+    #     editar_produto = FormularioProduto(instance=busca_produto)
+    # else:
+    #     produto_editado = FormularioProduto(request.POST, instance=busca_produto)
+    #     if produto_editado.is_valid():
+    #         produto_editado.save()
+    #         return redirect("pg_criar_produto")
+    # return render(request, "form-produtos.html", {"form_produto": editar_produto})
 
 #def ExcluirProduto(request, id_produto):
-    busca_produto = Produto.objects.get(id=id_produto)
-    if request.method == "POST":
-        busca_produto.delete()
-        return redirect("pg_criar_produto")
-    titulo_objeto = busca_produto.nome
-    return render(request, "conf-excluir.html", {"valor": titulo_objeto})
+    # busca_produto = Produto.objects.get(id=id_produto)
+    # if request.method == "POST":
+    #     busca_produto.delete()
+    #     return redirect("pg_criar_produto")
+    # titulo_objeto = busca_produto.nome
+    # return render(request, "conf-excluir.html", {"valor": titulo_objeto})
 
 def ibge (request):
     api = "https://Servicodados.ibge.gov.br/api/v1/localidades/estados/24/municipios"
@@ -391,7 +391,7 @@ def CriarEmpresa(request):
     }
     
     if request.method == "GET":
-        nova_empresa = FormularioEmpresa()
+        #nova_empresa = FormularioEmpresa()
 
         try:
             resposta = requests.get(url, headers=headers)
@@ -402,7 +402,7 @@ def CriarEmpresa(request):
     
         # Extraia a string desejada do JSON
         empresas = dados['empresas']
-        return render(request, "form-empresa.html", {"form_empresa": nova_empresa, "empresas": empresas})
+        return render(request, "form-empresa.html", { "empresas": empresas})
     else:
         # Dados que você deseja enviar no corpo da solicitação POST
         json = {
@@ -823,7 +823,7 @@ def CriarServico(request):
     empresas = dados_empresas['empresas']
     
     if request.method == "GET":
-        novo_servico = FormularioServico()
+       # novo_servico = FormularioServico()
 
         try:
             resposta = requests.get(url, headers=headers)
@@ -884,6 +884,70 @@ def ExcluirServico(request, id_servico):
                     print("A resposta não é um JSON válido.")
             else:
                 return HttpResponse('Erro ao consumir a API: ', response.status_code)
+            
+def EditarServico(request, id_servico):
+    url_editar_servico = 'http://127.0.0.1:9000/api/servicos/' + str(id_servico) # Substitua pela URL da API real
+    url_listar_servico = 'http://127.0.0.1:9000/api/servicos' # Substitua pela URL da API real
+    url_categorias = 'http://127.0.0.1:9000/api/categorias' 
+    url_empresas = 'http://127.0.0.1:9000/api/empresas' 
+
+    obter_token = RetornaToken(request)
+    conteudo_bytes = obter_token.content  # Obtém o conteúdo como bytes
+    token = conteudo_bytes.decode('utf-8') 
+
+    # Cabeçalhos que você deseja enviar com a solicitação
+    headers = {
+        'Authorization': 'Bearer ' + token,
+        'Content-Type': 'application/json'
+    }
+
+    resposta = requests.get(url_editar_servico, headers=headers)
+    resposta.raise_for_status()  # Levanta um erro para códigos de status HTTP 4xx/5xx
+    dados = resposta.json()
+    servico = dados['servico']
+
+    resposta_servico = requests.get(url_listar_servico, headers=headers)
+    resposta_servico.raise_for_status()  # Levanta um erro para códigos de status HTTP 4xx/5xx
+    dados_servicos = resposta_servico.json() # Obtém os dados JSON da resposta
+    servicos = dados_servicos['servicos']
+
+    resposta_categorias = requests.get(url_categorias, headers=headers)
+    resposta_categorias.raise_for_status()  # Levanta um erro para códigos de status HTTP 4xx/5xx
+    dados_categorias = resposta_categorias.json() # Obtém os dados JSON da resposta
+    categorias = dados_categorias['categorias']
+
+    resposta_empresas = requests.get(url_empresas, headers=headers)
+    resposta_empresas.raise_for_status()  # Levanta um erro para códigos de status HTTP 4xx/5xx
+    dados_empresas = resposta_empresas.json() # Obtém os dados JSON da resposta
+    empresas = dados_empresas['empresas']
+
+    if request.method == "GET":
+        return render(request, "form-servico.html", {"servico":servico,'categorias':categorias, 'empresas':empresas ,'servicos' : servicos}) 
+    else:
+        # Dados que você deseja enviar no corpo da solicitação POST
+        json = {
+            'tipo': request.POST['tipo'],
+            'valor': request.POST['valor'],
+            'empresa_id': request.POST['empresa_id'],
+            'categoria_id': request.POST['categoria_id'],
+            
+        }
+               
+        # Fazendo a solicitação POST
+        response = requests.put(url_editar_servico, json=json, headers=headers)
+        
+
+        # Obtendo o conteúdo da resposta
+        
+        if response.status_code in [200, 201]:
+            try:
+                return redirect("pg_criar_servico")
+            except requests.JSONDecodeError:
+                print("A resposta não é um JSON válido.")
+        else:
+            return render(request, "form-servico.html", {"servico": servico,'categorias':categorias, 'empresas':empresas , 'servicos' : servicos}) 
+        
+            
         
 '''def CriarOrdemServico(request):
     url = 'http://127.0.0.1:9000/api/ordemservicos' # Substitua pela URL da API real
